8e3982cc2179606055fa8af9d0090178106609f6
Tung <tungbtruong@gmail.com>
2020-03-29 14:57:31 -0600

    Working React App

--- experiencebu/src/index.js
+++ experiencebu/src/index.js
@@ -5,18 +5,13 @@ import {Provider} from "react-redux";
 import {createStore} from "redux";
 
 
-import App from "./components/App";
-import reducers from "./reducers";
-
-
-//code for switch statement navigation here?
-
+import App from "./App";
 
 
 
 
 ReactDOM.render(
-<Provider store = {createStore(reducers)}>
+<Provider>
 <App />
 </Provider>, 
 document.querySelector("#root"));
