ba401c54ceeee1c0a64a23de83ce19de28e8b951
Tung <tungbtruong@gmail.com>
2020-03-29 23:46:25 -0600

    Finished NavBar slight spacing issue

---
+++ experiencebu/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Masthead from '../MastHead/MastHead.js'
+import '@patternfly/react-core/dist/styles/base.css';
+
+function App() {
+  return (
+    <div>
+      <Masthead></Masthead>
+    </div>
+  );
+}
+
+export default App;
--- experiencebu/src/components/MastHead/MastHead.js
+++ experiencebu/src/components/MastHead/MastHead.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { Page, PageHeader, PageSidebar, PageSection, PageSectionVariants } from '@patternfly/react-core';
+import {
+  Nav,
+  NavExpandable,
+  NavItem,
+  NavItemSeparator,
+  NavList,
+  NavGroup,
+  NavVariants,
+  PageHeader,
+  Text
+} from '@patternfly/react-core';
+import "./MastHead.css"
 
-HorizontalPage = () => {
-  const logoProps = {
-    href: 'https://patternfly.org',
-    onClick: () => console.log('clicked logo'),
-    target: '_blank'
-  };
-  const Header = (
-    <PageHeader logo="Logo" logoProps={logoProps} toolbar="Toolbar" avatar=" | Avatar" topNav="Navigation" />
-  );
+class NavHorizontalList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 0
+    };
+    this.onSelect = result => {
+      this.setState({
+        activeItem: result.itemId
+      });
+    };
+  }
+  // ExperienceBU HOME EVENTS ORGANIZATIONS  Log In
+  render() {
+    const { activeItem } = this.state;
+    const nav = (
+      <Nav onSelect={this.onSelect}>
+        <div className='pf-c-page__header-nav'>
+        <NavList  variant={NavVariants.horizontal} > 
+          <Text className='title'> Experience </Text>
+          <Text className='BU'> BU </Text>
+          <NavItem></NavItem>
+          <NavItem className='home'> HOME </NavItem>
+          <NavItem className='events'> EVENTS </NavItem>
+          <NavItem className='organizations' > ORGANIZATIONS </NavItem>
+          <NavItem className='username'> Log In </NavItem>
+        </NavList>
+        </div>
+      </Nav>
+    );
+    return (
+      <PageHeader topNav={nav} style={{ backgroundColor: 'rgb(29, 28, 20)' }} />
+    );
+  }
+}
 
-  return (
-    <Page header={Header}>
-      <PageSection variant={PageSectionVariants.darker}>Section with darker background</PageSection>
-      <PageSection variant={PageSectionVariants.dark}>Section with dark background</PageSection>
-      <PageSection variant={PageSectionVariants.light}>Section with light background</PageSection>
-    </Page>
-  );
-};
+export default NavHorizontalList
