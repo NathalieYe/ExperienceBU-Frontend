ca4fe5ffd3cb639fc695035f705beadc8364ddb3
Tung Truong <tungbtruong@gmail.com>
2020-04-18 19:38:18 -0600

    Hooked up json-server with club spotlight ugly code

---
+++ experiencebu/src/components/Spotlight/Description.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text} from '@patternfly/react-core'
+
+const renderDescription = ({ clubs }) => {
+    console.log(Object.clubs)
+    console.log(typeof(clubs))
+    return (
+      <Text>Hello</Text>
+    )};
+
+export default renderDescription
--- experiencebu/src/components/Spotlight/Spotlight.js
+++ experiencebu/src/components/Spotlight/Spotlight.js
@@ -1,7 +1,8 @@
 import React from 'react'; 
-import { Grid, GridItem, Button } from '@patternfly/react-core';
+import { Grid, GridItem, Button,Text } from '@patternfly/react-core';
 import vsa from './vsa.png'
 import "./Spotlight.css"
+import Description from "./Description.js"
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,21 +10,80 @@ import {
   Link
 } from "react-router-dom";
 
+// console.log(this.state) 
+//console.log(this.state.clubs[0].description)
+
+
 export default class Spotlight extends React.Component {
-    render(){
+   constructor(props) {
+    super(props)
+    this.state = {
+      clubs: []
+    }
+  }
+
+   async componentWillMount() {
+      console.log("ComponentWillMount is called and data is being fetched")
+      const response = await fetch(`http://localhost:4000/clubs`);
+      const json = await response.json();
+      console.log("setState called")
+      this.setState({ clubs: json });
+      console.log("setState is finished")
+  }
+    
+/*
+    renderMyData() {
+      console.log("fetched called")
+      fetch('http://localhost:4000/clubs')
+      .then(res => res.json())
+      .then((data) => {
+        console.log("Data has been fetched")
+        this.setState({ clubs: data })
+       
+      })
+      .catch(console.log)
+    }
+  */
+
+
+    render() {
+      console.log("Render Method Called")
+      if (this.state.clubs[0]){
         return (
-            <Grid>
+          <Grid>
+          <GridItem span={12} className = 'headert' >Organization Spotlight</GridItem>
+          <GridItem span={1}></GridItem>
+          <GridItem span={3} rowSpan={4}>
+            <img src={vsa}/>
+          </GridItem>
+          <GridItem span={7} rowSpan={3}>
+           {this.state.clubs[0].description}
+          </GridItem>
+          <GridItem span={1}></GridItem>
+          <GridItem span={6}></GridItem>
+          <GridItem span={1}>  <Link to="/clubInfoPage"> <Button variant="link" isInline>
+            Learn More
+          </Button>   
+         </Link>
+          </GridItem>
+          <GridItem span={1}> </GridItem>
+          <GridItem span={12} className = 'bottom'></GridItem>
+        </Grid>
+        )
+    }
+
+    return (
+    <Grid>
     <GridItem span={12} className = 'headert' >Organization Spotlight</GridItem>
     <GridItem span={1}></GridItem>
     <GridItem span={3} rowSpan={4}>
       <img src={vsa}/>
     </GridItem>
     <GridItem span={7} rowSpan={3}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     <Description clubs={this.state.clubs} ></Description>
     </GridItem>
     <GridItem span={1}></GridItem>
     <GridItem span={6}></GridItem>
-   
     <GridItem span={1}>  <Link to="/clubInfoPage"> <Button variant="link" isInline>
       Learn More
     </Button>   
@@ -37,6 +97,7 @@ export default class Spotlight extends React.Component {
         );
         
     }
-    
 }
+    
+
 
